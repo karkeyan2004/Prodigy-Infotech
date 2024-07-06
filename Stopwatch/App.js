62781904a9322e3d5bf0d3acbdbd122ed2e99743
Karthikeyan S <123053390+karkeyan2004@users.noreply.github.com>
2024-07-06 21:12:45 +0530

    Create App.js

---
+++ Stopwatch/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Stopwatch from './Stopwatch';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Stopwatch />
+    </div>
+  );
+};
+
+export default App;
